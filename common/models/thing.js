46642439eaa9412d236b8a331da9cb7fc27f92e4
Richard Walker <digitalsadhu@gmail.com>
2015-07-29 22:41:57 +0200

    Set dates and user on thing create and update

--- common/models/thing.js
+++ common/models/thing.js
@@ -1,3 +1,20 @@
+var loopback = require('loopback');
+
 module.exports = function(Thing) {
 
+  Thing.observe('before save', function addUser(ctx, next) {
+    if (ctx.isNewInstance) {
+      ctx.instance.userId = loopback.getCurrentContext().get('currentUser').id;
+    }
+    next();
+  });
+
+  Thing.observe('before save', function addTimestampMetadata(ctx, next) {
+    if (ctx.isNewInstance)
+      ctx.instance.createdAt = ctx.instance.updatedAt = new Date();
+
+    if (ctx.currentInstance) ctx.currentInstance.updatedAt = new Date();
+
+    next();
+  });
 };
